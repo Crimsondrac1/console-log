023f89ca48910232f201aa888d1e667c2292c1cd
crimsondrac1 <doug@chillopolis.com>
2022-08-03 21:36:35 -0500

    Heroku ts #4

--- client/src/backendUrl.js
+++ client/src/backendUrl.js
@@ -1,4 +1,4 @@
 // const backendUrl = 'http://localhost:4000';
-const backendUrl = 'http://does-not-equal.herokuapp.com';
+const backendUrl = 'https://does-not-equal.herokuapp.com/';
 
 export default backendUrl;
