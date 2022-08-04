4a4bd321f0fe29ce94faa1eb6dcd823a09054787
crimsondrac1 <doug@chillopolis.com>
2022-08-04 18:55:25 -0500

    Heroku trblshooting 1

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const connectToDB = require('./connect');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { PORT } = require('./utils/config');
-
+// const { PORT } = require('./utils/config');
+const PORT = process.env.PORT || 4000
 connectToDB();
 
 const server = new ApolloServer({
@@ -12,6 +12,6 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
-server.listen({ port: PORT || 4000 }).then(({ url }) => {
+server.listen( PORT ).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
